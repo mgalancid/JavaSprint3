1cab1edf4ac8d1b9dde35596b4397c736f088833
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-13 08:44:10 -0300

    Added AuthController

---
+++ todolist/src/main/java/com/mindhub/todolist/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.mindhub.todolist.controllers;
+
+import com.mindhub.todolist.config.JwtUtils;
+import com.mindhub.todolist.models.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginUser loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.email(),
+                        loginRequest.password()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtil.generateToken(authentication.getName());
+        return ResponseEntity.ok(jwt);
+    }
+}
