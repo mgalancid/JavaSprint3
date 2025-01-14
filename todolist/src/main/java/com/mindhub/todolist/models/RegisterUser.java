4dfaf0afeb29a67aeb747861dcc980af562798a4
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-14 16:42:14 -0300

    Added verification

--- todolist/src/main/java/com/mindhub/todolist/models/RegisterUser.java
+++ todolist/src/main/java/com/mindhub/todolist/models/RegisterUser.java
@@ -1,4 +1,8 @@
 package com.mindhub.todolist.models;
 
-public record RegisterUser(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record RegisterUser(@NotNull @Email String email, @NotNull @Size(min=8) String password) {
 }
